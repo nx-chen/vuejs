2bf1a2e877c357c7d278eed459e842552729320c
Ningxi Chen <ningxi.chen@epita.fr>
2022-06-05 21:22:45 +0200

    finish set up

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <NavbarComponent />
+  </div>
+</template>
+
+<script>
+
+// @ is an alias to /src
+import NavbarComponent from '@/components/NavbarComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavbarComponent
+  }
+}
+</script>
